099c330b4498c060436737183235ea530ee6acf2
Elaine Cadman <elaine.cadman@gmail.com>
2018-09-30 20:50:04 -0700

    functional

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+ 
+    
+    
+    var computerChoices = (() => {
+      var caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+      return caps.map(letter => letter.toLowerCase());
+  
+
+      
+  })();
+  
+       // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+      var guessesRemaining = 9;
+      var wins = 0;
+      var losses = 0;
+      var pastGuesses = "";
+          
+      // Create variables that hold references to the places in the HTML where we want to display things.
+      var directionsText = document.getElementById("directions-text");
+      var userChoiceText = document.getElementById("userchoice-text");
+      var computerChoiceText = document.getElementById("computerchoice-text");
+      var guessesRemainingText  = document.getElementById("guessesRemaining-text");
+      var winsText = document.getElementById("wins-text");
+      var lossesText = document.getElementById("losses-text");
+      var pastGuessesText = document.getElementById("pastGuesses-text");
+  
+       // This function is run whenever the user presses a key.
+       document.onkeyup = function(event) {
+  
+  // Determines which key was pressed.
+  var userGuess = event.key;
+  pastGuesses=pastGuesses + event.key + ", ";
+  console.log(pastGuesses);
+  
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  
+   if (userGuess === computerGuess) {
+            wins++;
+            guessesRemaining=9;
+            pastGuesses="";
+          } else if (userGuess !== computerGuess) {
+            guessesRemaining--;
+          }
+  if (guessesRemaining < 1){
+      losses++;
+      guessesRemaining=9;
+      pastGuesses="";
+  
+  }
+  
+   // Display the user and computer guesses, and wins/losses/ties.
+
+          userChoiceText.textContent = "You chose: " + userGuess;
+          computerChoiceText.textContent = "The computer chose: " + computerGuess;
+          pastGuessesText.textContent = "Your previous guesses: " + pastGuesses;
+          guessesRemainingText.textContent = "Your guesses remaining: " + guessesRemaining;
+          winsText.textContent = "wins: " + wins;
+          lossesText.textContent = "losses: " + losses;
+
+      };
+  
