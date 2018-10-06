a428953a15d905769b80416a0e2d2635150e0af5
Elaine Cadman <elaine.cadman@gmail.com>
2018-10-06 16:44:25 -0700

    no really this time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,10 @@ var losses = 0;
 var pastGuesses = [];
 
 
-var computerChoices = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var userChoices = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//Array of choices for computer and limits user guesses to letters
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 
@@ -24,75 +26,58 @@ var pastGuessesText = document.getElementById("pastGuesses-text");
 // Randomly chooses a choice from the options array. This is the Computer's guess.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-console.log(computerGuess);
 //START OF GAME
 
 pastGuessesText.textContent = "";
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
-  // console.log("computer guess: " + computerGuess);
-   // Determines which key was pressed.
-   var userGuess = event.key;
-if (userChoices.includes(userGuess)){
-   if (!pastGuesses.includes(userGuess))  {
-    // pastGuesses.push(userGuess);
-    pastGuessesText.textContent = pastGuesses;
-    guessesRemaining = (guessesRemaining - 1);
-}
-   if(pastGuesses.includes(computerGuess)){
-     pastGuesses = "";
-   }
-   console.log(pastGuesses);
-   computerChoiceText.textContent = "";
+//reads key press
+  var userGuess = event.key;
+//limits user choice to letters
+  if (userChoices.includes(userGuess)) {
+//deducts from remaining guesses and logs guess
+    if (!pastGuesses.includes(userGuess)) {
+      pastGuessesText.textContent = pastGuesses;
+      guessesRemaining = (guessesRemaining - 1);
+    }
+//excludes past guesses from taking away from remaining guesses
+    if (pastGuesses.includes(computerGuess)) {
+      pastGuesses = "";
+    }
+//way to win!
+    if (userGuess === computerGuess) {
+      wins++;
+      guessesRemaining = 10;
+      alert("OMG you're like so psychic! I was totally thinking " + computerGuess + ". Let's see if you can do it again.");
+      computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+      pastGuesses = "";
+    } else if (!pastGuesses.includes(userGuess)) {
+      pastGuesses = pastGuesses + event.key + ", ";
+    }
+//deducts from guesses
+    if (userGuess !== computerGuess) {
+      guessesRemaining--;
+    }
+//allows for duplicate guesses
+    if (pastGuesses.includes(userGuess)) {
+      guessesRemaining++;
+    }
+//way to lose :(
+    if (guessesRemaining < 1) {
+      losses++;
+      alert("The computer chose: " + computerGuess + ", better luck next time!");
+      guessesRemaining = 9;
+      pastGuesses = "";
+      computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
   
-//    var inGuessArray = pastGuesses.includes(userGuess);
-//    console.log(inGuessArray);
-// if( guessesRemaining === 9){
-//   pastGuesses = "";
-// }
 
-   if (userGuess === computerGuess) {
-     wins++;
-     guessesRemaining = 9;
-     alert("OMG you're like so psychic! I was totally thinking " + computerGuess + ". Let's see if you can do it again.");
-     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
-     pastGuesses = "";
-
-   
-    } 
-
-
-    else if(!pastGuesses.includes(userGuess)){
-        pastGuesses = pastGuesses + event.key + ", ";
-        }
-     
-   if (userGuess !== computerGuess){
-     guessesRemaining--;
-     } 
-     if (pastGuesses.includes(userGuess)){
-         guessesRemaining++;
-     }
-   
-   if (guessesRemaining < 1) {
-     losses++;
-     alert( "The computer chose: " + computerGuess + ", better luck next time!");
-     guessesRemaining = 9;
-     pastGuesses = "";
-     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-   }
-  //  computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-// Display the user and computer guesses, and wins/losses/ties.
-
-
-userChoiceText.textContent = "You chose: " + userGuess;
-pastGuessesText.textContent = "Your previous guesses: " + pastGuesses;
-
-console.log("past", pastGuesses);
-guessesRemainingText.textContent = "Your guesses remaining: " + guessesRemaining;
-winsText.textContent = "wins: " + wins;
-lossesText.textContent = "losses: " + losses;
+    // Display the user and computer guesses, and wins/losses/ties.
+    userChoiceText.textContent = "You chose: " + userGuess;
+    pastGuessesText.textContent = "Your previous guesses: " + pastGuesses;
+    guessesRemainingText.textContent = "Your guesses remaining: " + guessesRemaining;
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
   }
 };
-
